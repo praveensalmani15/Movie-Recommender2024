d402d288bec495cd6b4e9c6fe145ee69feeaeb83
praveensalmani <praveensalmani.15.9.99@gmail.com>
2024-09-26 14:35:53 +0530

    Final-Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+# Build a Recommender System
+import streamlit as st
+from langchain import PromptTemplate, LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
+
+# Import the Local Environment
+from dotenv import load_dotenv
+import os 
+genai.configure(api_key=os.getenv('GOOGLE-API-KEY'))
+
+#Design the Page...  streamlit run app.py
+headers={"authorization":st.secrets['GOOGLE-API-KEY'],"Content-type":"application/json"}
+
+#Design the Page...
+st.title("Movie Recommeder Systems...")
+user_input=st.text_input('Enter the Movie title,genre or keyword')
+
+# Prompt Template
+demo_template='''Based on {user_input} provides Movie Recommendation'''
+template=PromptTemplate(input_variable=['user_input'],template=demo_template)
+
+# Google Gemini Model
+llm=ChatGoogleGenerativeAI(model='gemini-pro',api_key='GOOGLE-API-KEY ')
+
+if user_input:
+    prompt=template.format(user_input=user_input)
+    recommendations=llm.predict(text=prompt)
+    st.write(f'Recommendations for you : \n {recommendations}')
+else:
+    st.write('Please enter the Movie Name')
